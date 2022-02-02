a3ed541f619d20b1888d0d43fc9db25a5cad8b58
mgecmez <gecmez.melih@gmail.com>
2022-02-02 23:42:22 +0300

    initial commit

---
+++ WebScraping/Consts.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraping
+{
+    public static class Consts
+    {
+        public const string LoginCheck = @"
+            (function () {
+                if (document.querySelector('.header-signin') == null) {
+                    console.log('user is logged in');
+                } else {
+                    console.log('user is not logged in');
+                }
+            })();
+        ";
+
+        public const string RedirectLogin = @"
+            (function () {
+                if (document.querySelector('.header-signin') != null) {
+                    console.log('redirect login test');
+                    document.querySelector('.header-signin').click();
+                } else {
+                    //console.log('element is null');
+                    //console.log('user is logged in');
+                }
+            })();
+        ";
+
+        public const string Login = @"
+            (function () {
+                document.querySelector('#email').value = 'johngerson808@gmail.com';
+                document.querySelector('#password').value = 'test8008';
+                document.querySelector('.sds-button').click();
+            })();
+        ";
+
+        public const string Logout = @"
+            (function () {
+                if (document.querySelector('.header-signout') != null) {
+                    console.log('user will be logout');
+                    document.querySelector('.header-signout').click();
+                }
+            })();
+        ";
+
+        public const string SearchOnHomePage = @"
+            (function () {
+                function triggerEvent(el, type) {
+                    // IE9+ and other modern browsers
+                    if ('createEvent' in document) {
+                        var e = document.createEvent('HTMLEvents');
+                        e.initEvent(type, false, true);
+                        el.dispatchEvent(e);
+                    } else {
+                        // IE8
+                        var e = document.createEventObject();
+                        e.eventType = type;
+                        el.fireEvent('on' + e.eventType, e);
+                    }
+                }
+
+                const elmStockType = document.querySelector('#make-model-search-stocktype');
+                elmStockType.value = 'used';
+                triggerEvent(elmStockType, 'change');
+
+                const elmMakes = document.querySelector('#makes');
+                elmMakes.value = 'tesla';
+                triggerEvent(elmMakes, 'change');
+
+                const elmModels = document.querySelector('#models');
+                elmModels.value = 'tesla-model_s';
+                triggerEvent(elmModels, 'change');
+
+                const elmPrice = document.querySelector('#make-model-max-price');
+                elmPrice.value = '100000';
+                triggerEvent(elmPrice, 'change');
+
+                const elmDistance = document.querySelector('#make-model-maximum-distance');
+                elmDistance.value = 'all';
+                triggerEvent(elmDistance, 'change');
+
+                const elmZipCode = document.querySelector('#make-model-zip');
+                elmZipCode.value = '94596';
+
+                const formSearch = document.querySelector('.search-form');
+                formSearch.submit();
+
+                return 'test';
+            })();
+        ";
+
+        public const string GetVehicleDatas = @"
+            (function () {
+                var cards = document.querySelectorAll('.vehicle-card');
+                var result = [];
+
+                //var carIndexesWithHomeDelivery = [];
+                //cards.forEach((item, index) => {
+                //    if (item.querySelector('.vehicle-details .vehicle-badging .sds-badge--home-delivery') != null) {
+                //        carIndexesWithHomeDelivery.push(index);
+                //    }
+                //});
+                //var randomCarIndex = carIndexesWithHomeDelivery[Math.floor(Math.random() * carIndexesWithHomeDelivery.length)];
+
+                for (var i = 0; i < cards.length; i++) {
+                    const id = cards[i].id;
+                    const elm = document.getElementById(id);
+
+                    const images = [];
+                    const elmImages = elm.querySelectorAll('.gallery-wrap .image-wrap > img');
+                    for (var j = 0; j < elmImages.length; j++) {
+                        const elmImage = elmImages[j];
+                        images.push({
+                            title: elmImage.alt,
+                            url: elmImage.src
+                        });
+                    }
+
+                    const stockType = elm.querySelector('.vehicle-details .stock-type') != null ? elm.querySelector('.vehicle-details .stock-type').innerHTML : '';
+                    const title = elm.querySelector('.vehicle-details h2.title') != null ? elm.querySelector('.vehicle-details h2.title').innerHTML : '';
+                    const mileage = elm.querySelector('.vehicle-details .mileage') != null ? elm.querySelector('.vehicle-details .mileage').innerHTML : '';
+                    const price = elm.querySelector('.vehicle-details .primary-price') != null ? elm.querySelector('.vehicle-details .primary-price').innerHTML : '';
+                    const dealerName = elm.querySelector('.vehicle-details .dealer-name strong') != null ? elm.querySelector('.vehicle-details .dealer-name strong').innerHTML : '';
+                    const rating = elm.querySelector('.vehicle-details .sds-rating .sds-rating__count') != null ? elm.querySelector('.vehicle-details .sds-rating .sds-rating__count').innerHTML : '';
+                    const review = elm.querySelector('.vehicle-details .sds-rating .sds-rating__link') != null ? elm.querySelector('.vehicle-details .sds-rating .sds-rating__link').innerHTML.replace('(', '').replace(')', '').replace(' reviews', '') : '';
+                    const milesFrom = elm.querySelector('.vehicle-details .miles-from') != null ? elm.querySelector('.vehicle-details .miles-from').innerHTML : '';
+
+                    //var highlights = [];
+                    //if (i === randomCarIndex) {
+                    //    elm.querySelector('.vehicle-details .vehicle-badging .sds-badge--home-delivery').click();
+                    //    var divHighlights = document.querySelectorAll('#sds-modal ul li > div');
+                    //    for (var i = 0; i < divHighlights.length; i++) {
+                    //        var highlight = divHighlights[i];
+                    //        var desc = highlight.getElementsByClassName('badge-description')[0].innerText;
+                    //        if (highlight.className == 'home_delivery-badge') {
+                    //            highlights.push({
+                    //                type: 'home-delivery',
+                    //                description: desc
+                    //            });
+                    //        }
+                    //        else if (highlight.className == 'virtual_appointments-badge') {
+                    //            highlights.push({
+                    //                type: 'virtual-appointments',
+                    //                description: desc
+                    //            });
+                    //        }
+                    //        else if (highlight.className == 'hot_car-badge') {
+                    //            highlights.push({
+                    //                type: 'hot-car',
+                    //                description: desc
+                    //            });
+                    //        }
+                    //        else if (highlight.className == 'price-badge') {
+                    //            var priceDetailType = highlight.getElementsByClassName('sds-badge')[0];
+                    //            if (priceDetailType.className.indexOf('sds-badge-fair') > -1) {
+                    //                highlights.push({
+                    //                    type: 'fair-deal',
+                    //                    description: desc
+                    //                });
+                    //            }
+                    //            else if (priceDetailType.className.indexOf('sds-badge-good') > -1) {
+                    //                highlights.push({
+                    //                    type: 'good-deal',
+                    //                    description: desc
+                    //                });
+                    //            }
+                    //            else if (priceDetailType.className.indexOf('sds-badge-great') > -1) {
+                    //                highlights.push({
+                    //                    type: 'great-deal',
+                    //                    description: desc
+                    //                });
+                    //            }
+                    //        }
+                    //    }
+                    //    document.querySelector('#badge-modal-close').click();
+                    //}
+
+                    result.push({
+                        id,
+                        images,
+                        stockType,
+                        title,
+                        mileage,
+                        price,
+                        dealerName,
+                        rating,
+                        review,
+                        milesFrom
+                    });
+                }
+                return result;
+            })();
+        ";
+
+        public const string RedirectSecondPage = @"
+            (function () {
+                if (document.querySelector('#next_paginate') != null) {
+                    document.querySelector('#next_paginate').click();
+                }
+            })();
+        ";
+
+        public const string ChangeSearch = @"
+            (function () {
+                const elmMakes = document.querySelectorAll('.sds-field.available [name=\'models[]\']')
+                elmMakes[1].checked = false;
+                elmMakes[2].click();
+            })();
+        ";
+    }
+}
---
+++ WebScraping/Program.cs
@@ -0,0 +1,227 @@
+﻿using CefSharp;
+using CefSharp.OffScreen;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebScraping
+{
+    class Program
+    {
+        private static ChromiumWebBrowser browser;
+
+        static string currentUrl = "";
+        static readonly string homeUrl = "https://www.cars.com/";
+        static readonly string loginUrl = "https://www.cars.com/signin/?redirect_path=%2F";
+        static readonly string logoutUrl = "https://www.cars.com/signout/?redirect_path=%2F";
+        static readonly string searchUrl = "https://www.cars.com/shopping/results";
+
+        static bool isInit = false;
+        static bool isLoggedIn = false;
+        static bool isRedirectHomeToSearch = false;
+        static bool isRedirectToSearchPage2 = false;
+        static bool lastStep = false;
+        static bool isFirstSearchRun = false;
+        static bool isSecondSearchRun = false;
+        static bool isFinished = false;
+        static bool isProcessing = true;
+
+        static List<object> vehicles = new List<object>();
+        static string jsonData = "";
+
+        static void Main(string[] args)
+        {
+            CefRuntime.SubscribeAnyCpuAssemblyResolver();
+
+            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+
+            var settings = new CefSettings()
+            {
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp\\Cache")
+            };
+
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            browser = new ChromiumWebBrowser(homeUrl);
+
+            browser.AddressChanged += Browser_AddressChanged;
+            browser.LoadingStateChanged += Browser_LoadingStateChanged;
+            browser.ConsoleMessage += Browser_ConsoleMessage;
+
+            Console.WriteLine("   ╔═══════════════════════════════════════════════════╗");
+            Console.WriteLine("   ║             WEB-SCRAPING-BOT ACTIVATED            ║");
+            Console.WriteLine("   ╚═══════════════════════════════════════════════════╝");
+
+            while (isProcessing)
+            {
+                Thread.Sleep(1000);
+            }
+
+            Cef.Shutdown();
+        }
+
+        private static void Browser_AddressChanged(object sender, AddressChangedEventArgs e)
+        {
+            if (currentUrl == "") { }
+            else if (currentUrl == homeUrl && e.Address == loginUrl)
+            {
+                isLoggedIn = false;
+            }
+            else if (currentUrl == loginUrl && e.Address == homeUrl)
+            {
+                isLoggedIn = true;
+            }
+            else if (currentUrl == logoutUrl && e.Address == homeUrl)
+            {
+                isLoggedIn = false;
+            }
+            else if (currentUrl == homeUrl && e.Address.Contains(searchUrl))
+            {
+                isRedirectHomeToSearch = true;
+            }
+            else if (e.Address.Contains(searchUrl) && isRedirectToSearchPage2 && !e.Address.Contains("page=2"))
+            {
+                isRedirectHomeToSearch = true;
+                isRedirectToSearchPage2 = false;
+                isFirstSearchRun = false;
+                isSecondSearchRun = false;
+                lastStep = true;
+            }
+            else if (e.Address.Contains(searchUrl) && e.Address.Contains("page=2"))
+            {
+                isRedirectHomeToSearch = false;
+                isRedirectToSearchPage2 = true;
+            }
+
+            currentUrl = e.Address;
+        }
+
+        private static void Browser_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (!e.IsLoading)
+            {
+                if (isFinished)
+                {
+                    Console.WriteLine(jsonData);
+                    Console.WriteLine("process finished");
+                    Console.ReadLine();
+                    isProcessing = false;
+                }
+                else if (currentUrl == homeUrl && !isInit)
+                {
+                    var loginCheck = browser.EvaluateScriptAsync(Consts.LoginCheck);
+                    loginCheck.ContinueWith(x =>
+                    {
+                        var responseLoginCheck = x.Result;
+                        if (responseLoginCheck.Success)
+                        {
+                            isInit = true;
+                            if (isLoggedIn)
+                            {
+                                var logout = browser.EvaluateScriptAsync(Consts.Logout);
+                                logout.ContinueWith(y =>
+                                {
+                                    var responseLogout = y.Result;
+                                    if (responseLogout.Success) { }
+                                });
+                            }
+                        }
+                    });
+                }
+                else if (currentUrl == homeUrl && isInit && !isLoggedIn)
+                {
+                    var redirectLogin = browser.EvaluateScriptAsync(Consts.RedirectLogin);
+                    redirectLogin.ContinueWith(x =>
+                    {
+                        var response = x.Result;
+                        if (response.Success) { }
+                    });
+                }
+                else if (currentUrl == loginUrl && !isLoggedIn)
+                {
+                    var login = browser.EvaluateScriptAsync(Consts.Login);
+                    login.ContinueWith(x =>
+                    {
+                        var response = x.Result;
+                        if (response.Success) { }
+                    });
+                }
+                else if (currentUrl == homeUrl && isLoggedIn)
+                {
+                    var searchOnHomePage = browser.EvaluateScriptAsync(Consts.SearchOnHomePage);
+                    searchOnHomePage.ContinueWith(x =>
+                    {
+                        var response = x.Result;
+                        if (response.Success) { }
+                    });
+                }
+                else if (currentUrl.Contains(searchUrl) && isRedirectHomeToSearch)
+                {
+                    var vehicleDatas = browser.EvaluateScriptAsync(Consts.GetVehicleDatas);
+                    vehicleDatas.ContinueWith(x =>
+                    {
+                        var responseVehicleDatas = x.Result;
+                        if (responseVehicleDatas.Success && responseVehicleDatas.Result != null && !isFirstSearchRun)
+                        {
+                            isFirstSearchRun = true;
+                            vehicles.AddRange((List<object>)responseVehicleDatas.Result);
+
+                            var redirectSecondPage = browser.EvaluateScriptAsync(Consts.RedirectSecondPage);
+                            redirectSecondPage.ContinueWith(y =>
+                            {
+                                var responseRedirectSecondPage = y.Result;
+                                if (responseRedirectSecondPage.Success) { }
+                            });
+                        }
+                    });
+                }
+                else if (currentUrl.Contains(searchUrl) && isRedirectToSearchPage2)
+                {
+                    var vehicleDatas = browser.EvaluateScriptAsync(Consts.GetVehicleDatas);
+                    vehicleDatas.ContinueWith(x =>
+                    {
+                        var responseVehicleDatas = x.Result;
+                        if (responseVehicleDatas.Success && responseVehicleDatas.Result != null && !isSecondSearchRun)
+                        {
+                            isSecondSearchRun = true;
+                            var result = (List<object>)responseVehicleDatas.Result;
+                            vehicles.AddRange(result);
+
+                            if (!lastStep)
+                            {
+                                var changeSearch = browser.EvaluateScriptAsync(Consts.ChangeSearch);
+                                changeSearch.ContinueWith(y => {
+                                    var responseChangeSearch = y.Result;
+                                    if (responseChangeSearch.Success) { }
+                                });
+                            }
+                            else
+                            {
+                                jsonData = JsonConvert.SerializeObject(vehicles);
+                                isFinished = true;
+                            }
+                        }
+                    });
+                }
+            }
+        }
+
+        private static void Browser_ConsoleMessage(object sender, ConsoleMessageEventArgs e)
+        {
+            if (e.Message.Contains("user is logged in"))
+            {
+                isLoggedIn = true;
+            }
+            else if (e.Message.Contains("user is not logged in"))
+            {
+                isLoggedIn = false;
+            }
+            else if (e.Message.Contains("user will be logout"))
+            {
+                isLoggedIn = false;
+            }
+        }
+    }
+}
